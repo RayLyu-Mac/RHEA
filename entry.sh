113a46c3666b29767a1a9b0144fad354c9885488
raylyu <59774755+RayLyu-Mac@users.noreply.github.com>
2024-10-01 11:58:25 +0000

    a

--- entry.sh
+++ entry.sh
@@ -6,5 +6,6 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
+echo "===================="
 pip install /app/requirements.txt
 python3 /app/S1_FeatureCal/autoTest.py
