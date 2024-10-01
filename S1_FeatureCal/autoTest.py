cf4ce4aa6e3854290aa72c4083e95b35b435cf26
raylyu <59774755+RayLyu-Mac@users.noreply.github.com>
2024-10-01 10:29:37 +0000

    c

--- S1_FeatureCal/autoTest.py
+++ S1_FeatureCal/autoTest.py
@@ -7,6 +7,6 @@ def get_chemicals_list(filename):
     chemicals_list = list(chemicals[' symbol'].str.strip())
     return chemicals_list
 script_dir = os.path.dirname(os.path.realpath(__file__))
-chemListDir=csv_file_path = os.path.join(script_dir, 'your_file.csv')
+chemListDir=csv_file_path = os.path.join(script_dir, 'chemical_elements.csv')
 chem_list=get_chemicals_list(chemListDir)
 print(chem_list)
